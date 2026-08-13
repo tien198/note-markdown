8f5079298bfda9f672435926745134261cae2d77
tien998 <tienvn998@gmail.com>
2026-08-14 02:21:17 +0700

    Fix Vercel deployment with Nitro preset

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,14 @@ import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
+import { nitro } from 'nitro/vite'
 
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
   resolve: { tsconfigPaths: true },
-  plugins: [
-    devtools(),
-    tailwindcss(),
-    tanstackStart({
-      prerender: {
-        enabled: true,
-        crawlLinks: true,
-      },
-    }),
-    viteReact(),
-  ],
+  plugins: [devtools(), tailwindcss(), tanstackStart(), nitro(), viteReact()],
   server: {
     open: true,
   },
